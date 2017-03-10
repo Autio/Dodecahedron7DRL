5f6e94f51fde801eed5dc6244f04a935bce2bd7b
Petri Autio <petri.mikael.autio@gmail.com>
2017-03-10 21:05:49 +0000

    Keyboard navigation
    And generated dots to orient pentagonal faces

---
+++ Dodecahedron7DRL/Assets/Scripts/FaceScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceScript : MonoBehaviour {
+    public GameObject marker;
+    public float distance = 1f;
+    public float zOffset = 0f;
+	// Use this for initialization
+	void Start () {
+        GameObject newMarker = GameObject.Instantiate(marker, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation, this.transform) as GameObject;
+        newMarker.transform.Translate(Vector3.up * -distance);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Dodecahedron7DRL/Assets/Scripts/GameController.cs
+++ Dodecahedron7DRL/Assets/Scripts/GameController.cs
@@ -13,17 +13,17 @@ public class GameController : MonoBehaviour {
 	void Start () {
         // define tile network
         tileLinks.Add(new int[] { 2, 3, 4, 5, 6}); // face 1
-        tileLinks.Add(new int[] { 1, 6, 10, 9, 3}); // face 2
-        tileLinks.Add(new int[] { 2, 9, 8, 4, 1}); // face 3
-        tileLinks.Add(new int[] { 1, 3, 8, 12, 5}); // face 4
-        tileLinks.Add(new int[] { 1, 4, 12, 11, 6}); // face 5
-        tileLinks.Add(new int[] { 1, 5, 11, 10, 2}); // face 6
+        tileLinks.Add(new int[] { 9, 3, 1, 10, 6}); // face 2
+        tileLinks.Add(new int[] { 8, 4, 1, 2, 9}); // face 3
+        tileLinks.Add(new int[] { 12, 5, 1, 3, 8}); // face 4
+        tileLinks.Add(new int[] { 11, 6, 1, 4, 12}); // face 5
+        tileLinks.Add(new int[] { 10, 2, 1, 5, 11}); // face 6
         tileLinks.Add(new int[] { 8, 9, 10, 11, 12}); // face 7
-        tileLinks.Add(new int[] { 7, 12, 4, 3, 9}); // face 8
-        tileLinks.Add(new int[] { 7, 8, 2, 3, 10}); // face 9
-        tileLinks.Add(new int[] { 7, 9, 2, 6, 11}); // face 10
-        tileLinks.Add(new int[] { 7, 10, 5, 6, 12}); // face 11
-        tileLinks.Add(new int[] { 7, 11, 5, 4, 8}); // face 12
+        tileLinks.Add(new int[] { 3, 9, 7, 12, 4}); // face 8
+        tileLinks.Add(new int[] { 2, 10, 7, 8, 3}); // face 9
+        tileLinks.Add(new int[] { 6, 11, 7, 9, 2}); // face 10
+        tileLinks.Add(new int[] { 5, 12, 7, 10, 9}); // face 11
+        tileLinks.Add(new int[] { 4, 8, 7, 11, 5}); // face 12
 
     }
 
@@ -77,7 +77,6 @@ public class GameController : MonoBehaviour {
         {
             targetFace = tileLinks[sourceFace][0];
         }
-        
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Keypad6))
         {
             targetFace = tileLinks[sourceFace][1];
